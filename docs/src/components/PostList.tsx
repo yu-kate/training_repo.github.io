e12808d509f6ec1733c439c4b85e9ae9b6ccee12
yu-kate <yu.katelyna@gmail.com>
2025-01-06 16:03:33 -0800

    moved notes, blog-app renamed to docs

--- docs/src/components/PostList.tsx
+++ docs/src/components/PostList.tsx
@@ -54,35 +54,4 @@ function Post({ content, index }: PostProps) {
     </div>;
 }
 
-// interface LikesProps {
-//   index: Number;
-// }
-// function LikesComponent({index}: LikesProps) {
-// const [likesCounter, setLikesCounter] = useState(() => {
-//     const savedLikes = getSavedLikes();
-//     return savedLikes;
-// });
-// function setSavedLikes() {
-//     localStorage.setItem(String("savedLikes"+index), JSON.stringify(likesCounter));
-// }
-// function getSavedLikes() {
-//     const savedLikes = JSON.parse(localStorage.getItem(String("savedLikes"+index)) || '0');
-//     if (savedLikes==null) return 0;
-//     return savedLikes;
-// }
-// useEffect(() => {
-//     setSavedLikes();
-// }, [likesCounter]);
-
-// return <div className="likes-counter">
-//     <button onClick={() => setLikesCounter(likesCounter+1)}>
-//     <img src="images/upvote-button.png" alt="upvote" className="like-button"/>
-//     </button>
-//     <span className="num-likes">{likesCounter>0 ? "+" : ''}{likesCounter}</span>
-//     <button onClick={() => setLikesCounter(likesCounter-1)}>
-//     <img src="images/downvote-button.png" alt="downvote" className="like-button"/>
-//     </button>
-// </div>
-// }
-
 export default PostList;
